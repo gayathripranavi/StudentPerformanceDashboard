e9f4da8e0b82fc18bb045ec985a1f21bb8f8c22e
gayathripranavi <gayathriprakashh94@gmail.com>
2025-08-29 12:32:20 -0700

    yes

--- src/components/SubjectChart.jsx
+++ src/components/SubjectChart.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import {
-  ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar
+  ResponsiveContainer,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar
 } from 'recharts';
 
 export default function SubjectChart({ data }) {
-  console.log(data);
   return (
-  <div className="chart-wrapper" style={{ width: '100%', height: 300 }}>
-    <ResponsiveContainer>
-      <BarChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="subject" angle={0} textAnchor="end" interval={0} />
-        <YAxis />
-        <Tooltip />
-        <Bar dataKey="avgScore" fill="#ff6600" name="Avg Score" />
-      </BarChart>
-    </ResponsiveContainer>
-  </div>
-);
-
+    <div className="chart-wrapper">
+      <ResponsiveContainer>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* ✅ make labels rotate for responsiveness */}
+          <XAxis 
+            dataKey="subject" 
+            angle={0} 
+            textAnchor="end" 
+            interval={0} 
+            height={60} 
+          />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="avgScore" fill="#ff6600" name="Avg Score" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
